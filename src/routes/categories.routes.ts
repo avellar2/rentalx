90a30897f60b72289cd82c8d92df11d25c5df302
Vanderson <vandimavellar1997@gmail.com>
2023-06-27 11:08:56 -0300

    Create Category Repository

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,21 +1,13 @@
 import { Router } from 'express'
-import { Category } from '../model/Category'
+import { CategoryRepository } from '../repositories/CategoriesRepository'
 
 export const categoriesRoutes = Router()
-
-const categories: Category[] = []
+const categoryRepository = new CategoryRepository()
 
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body
 
-  const category = new Category()
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date(),
-  })
-
-  categories.push(category)
+  categoryRepository.create({ name, description })
 
   return response.status(201).send()
 })
